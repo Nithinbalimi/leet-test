1b8f503a6cf08b81c956a2ec9b6c138f37885b08
BALIMI NITHIN <nithinbalimi@gmail.com>
2026-06-21 18:05:42 +0530

    Time: 36 ms (77.33%) | Memory: 84.1 MB (15.99%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count=0;
+        for(int i:costs){
+            if(coins>=i){
+                coins=coins-i;
+                count++;
+            }
+        }
+    return count;
+
+    }
+}
