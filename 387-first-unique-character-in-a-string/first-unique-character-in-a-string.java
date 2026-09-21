56e113e0e4cab2d00062e976bee2cd2889de73a8
BALIMI NITHIN <nithinbalimi@gmail.com>
2026-09-21 15:00:26 +0530

    Time: 44 ms (6.34%) | Memory: 47.1 MB (24.80%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,11 +1,25 @@
+import java.util.Map.*;
 class Solution {
     public int firstUniqChar(String s) {
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(s.indexOf(c)==s.lastIndexOf(c)){
-                return i;
+       LinkedHashMap<Character,Integer> map=new LinkedHashMap();
+       for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                int c=map.get(s.charAt(i));
+                map.put(s.charAt(i),c+1);
+            }
+            else{
+                map.put(s.charAt(i),1);
+            }
+       }
+       Set<Entry<Character,Integer>> e=map.entrySet();
+       for(Entry<Character,Integer> x : e){
+        if(x.getValue()==1){
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i)==x.getKey()){
+                    return i;
+                }
             }
         }
-        return -1;
+       }return -1;
     }
 }
